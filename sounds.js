854952f4943cdc58de5006794ad4a51af99a0fe8
Matheus Santos <matheussantosjjj@gmail.com>
2023-06-27 00:51:08 -0300

    "Sample Audios OK" Todos Bugs corrigidos

--- sounds.js
+++ sounds.js
@@ -1,24 +1,57 @@
-function playMusic(audioId) {
-    var audio = document.getElementById(audioId);
-    audio.play();
+//make a simple function that play and stop the music if a user check and unckeck the checkbox
+function playMusic1() {
+    var music = document.getElementById("audio1");
+    music.play();
+}
 
-    //make a function that stop the music when the checkbox is unchecked
-    function stopMusic() {
-        audio.pause();
-        audio.currentTime = 0;
-    }
-
-  }
+function pauseMusic1(){
+    var music = document.getElementById("audio1");
+    music.pause();
+}
 
 
-x
-  
-// make a function that valides if a user select two or more checkboxes and apperar alert message show "Error"
-function validate() {
-    var checkboxes = document.querySelectorAll('botao-seletor:checked');
-    if (checkboxes.length < 2) {
-        alert("Error");
-        return false;
-    }
-    return true;
+//
+
+function playMusic2() {
+    var music = document.getElementById("audio2");
+    music.play();
+}
+
+function pauseMusic2(){
+    var music = document.getElementById("audio2");
+    music.pause();
+}
+
+//
+
+function playMusic3() {
+    var music = document.getElementById("audio3");
+    music.play();
+}
+    
+function pauseMusic3(){
+    var music = document.getElementById("audio3");
+    music.pause();
+}
+
+//
+function playMusic4() {
+    var music = document.getElementById("audio4");
+    music.play();
+}
+        
+function pauseMusic4(){
+    var music = document.getElementById("audio4");
+    music.pause();
+}
+
+//
+function playMusic5() {
+    var music = document.getElementById("audio5");
+    music.play();
+}
+        
+function pauseMusic5(){
+    var music = document.getElementById("audio5");
+    music.pause();
 }
